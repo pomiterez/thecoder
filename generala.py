2317276b14ec1bbb5a112629bd06eb04909a1e86
Maq301 <transporteluke@gmail.com>
2018-10-10 19:31:02 -0300

    ok

---
+++ generala.py
@@ -0,0 +1,15 @@
+import random
+def dame_un_dado():
+    return random.randrange(1,7)
+def dame_tirada():
+    tirada = []
+    for dado in range (1,6):
+        tirada.append(dame_un_dado())
+    return tirada
+cantidad_jugadores = int(input("Bienvenido a la generala. Ingrese la cantidad de jugadores: "))
+tiros = 0
+for i in range (1,cantidad_jugadores):
+    tiros = tiros+1
+    tirada=dame_tirada()
+    print("Los dados que salieron son: ",tirada)
+    print(hola)
